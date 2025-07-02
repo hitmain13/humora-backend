0fbd043f8dc8843bbfbe8fcb1b7f205480576a22
Fabio <hide.fabio123@gmail.com>
2025-07-01 23:22:52 -0300

    start do server

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/authRoutes';
+import emotionRoutes from './routes/emotionRoutes';
+import { authenticateToken } from './middlewares/authMiddleware';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Humora Backend is running!');
+});
+
+app.use('/auth', authRoutes);
+app.use('/emotions', emotionRoutes);
+
+app.get('/protected', authenticateToken, (req, res) => {
+  res.json({ message: 'This is a protected route!', userId: (req as any).userId });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
